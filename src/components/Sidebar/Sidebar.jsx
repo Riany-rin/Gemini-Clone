796e1172b7869e0d1f0be9e87e9ddb42251bd332
Riany-rin <rianynurannisa@gmail.com>
2024-06-13 22:09:26 +0900

    Placeholder for sidebar top

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './Sidebar.css'
+import { assets } from '../../assets/assets'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      
+      <div className="top">
+        <img className='menu' src={assets.menu_icon} alt="Menu Icon" />
+        <div className="new-chat">
+            <img src={assets.plus_icon} alt="plus icon" />
+            <p>New Chat</p>
+        </div>
+        <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+                <img src={assets.message_icon} alt="message icon" />
+                <p>What is react ...</p>
+            </div>
+        </div>
+      </div>
+      <div className="bottom">
+
+      </div>
     </div>
   )
 }
