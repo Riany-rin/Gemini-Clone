490b7e4fdca071bcd1aefeb9cc7f09df31994b8d
Riany-rin <rianynurannisa@gmail.com>
2024-06-13 22:12:24 +0900

    Placeholder for bottom sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -20,7 +20,18 @@ const Sidebar = () => {
         </div>
       </div>
       <div className="bottom">
-
+        <div className="bottom-item recent-entry">
+            <img src={assets.question_icon} alt="question icon" />
+            <p>Help</p>
+        </div>
+        <div className="bottom-item recent-entry">
+            <img src={assets.history_icon} alt="history icon" />
+            <p>Activity</p>
+        </div>
+        <div className="bottom-item recent-entry">
+            <img src={assets.setting_icon} alt="setting icon" />
+            <p>Settings</p>
+        </div>
       </div>
     </div>
   )
